c0a900cd452e5ba64c78f47e4376707f6d4177b5
Shinewisely <delightwook@dcn.ssu.ac.kr>
2017-07-22 16:25:06 +0900

    creating Policy for LBAAS

--- vnfm/policy_actions/recovery/recovery.py
+++ vnfm/policy_actions/recovery/recovery.py
@@ -19,8 +19,10 @@ from tacker.db.common_services import common_services_db
 from tacker.plugins.common import constants
 from tacker.vnfm.policy_actions import abstract_action
 from tacker.vnfm import vim_client
-from tacker.nfvo import nfvo_plugin
-
+from tacker.nfvo.drivers.vim import openstack_driver
+from novaclient import client
+from keystoneauth1 import session
+from keystoneauth1.identity import v3
 
 LOG = logging.getLogger(__name__)
 
@@ -82,6 +84,9 @@ class VNFActionRecovery(abstract_action.AbstractPolicyAction):
 
         if cnt == 0 :
             self.job_id = plugin._start_restore_action(context,vim_res['vim_auth'],vnf_dict['instance_id'])
+            print(vim_res)
+            print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxx')
+            print(vnf_dict)
             cnt += 1
 
 
@@ -91,6 +96,65 @@ class VNFActionRecovery(abstract_action.AbstractPolicyAction):
         #
 
         # 1. add member
+        ### get new Nova IP address
+
+        auth = v3.Password(auth_url=vim_res['vim_auth']['auth_url'],username=vim_res['vim_auth']['username']
+                           ,password=vim_res['vim_auth']['password'],
+                           project_name=vim_res['vim_auth']['project_name'],
+                           user_domain_id=vim_res['vim_auth']['user_domain_name'],
+                           project_domain_id=vim_res['vim_auth']['project_domain_name'])
+        sess = session.Session(auth = auth)
+        novaclient=client.Client("2.1",session=sess)
+
+
+        ############################### Need Sujung ################################
+        ip = vnf_dict['mgmt_url'].split(':')
+        ###### ALL network Search
+
+        ############################### Need Sujung ################################
+
+        new_ip = ip[1].replace("}","")
+
+        result_ip = (new_ip.replace("\"","")).strip()
+
+        novadict = novaclient.servers.list(search_opts={'ip':str(result_ip)})
+
+        # ListWithMetadata Tyoe
+
+        novaname = novadict[0]
+        novahead=novaclient.servers.get(novaname)
+        print("#######################################################")
+        print("#######################################################")
+        print("#######################################################")
+        print(type(novahead))
+        print("#######################################################")
+        print("##########s#############################################")
+        print("#######################################################")
+        print("#######################################################")
+
+
+
+        # findnova = novaclient.servers.list(search_opts={'name':str(novaname)})
+
+
+
+        #
+        #neutronclient = openstack_drive
+        # r.NeutronClient(vim_res['vim_auth'])
+        #
+        # creatmemfeed = neutronclient.client.create_lbaas_member('test-lb-pool-socket',)
+
+
+        print("#########################################")
+        print("###########createmembfeed#################")
+        print("#########################################")
+        print("#########################################")
+        print("#########################################")
+
+
+
+
+
 
 
 
@@ -139,3 +203,5 @@ class VNFActionRecovery(abstract_action.AbstractPolicyAction):
         #         _delete_heat_stack(vim_res['vim_auth'])
         #         vnf_dict['attributes'].pop('alarming_policy')
         #         _respin_vnf()
+
+
